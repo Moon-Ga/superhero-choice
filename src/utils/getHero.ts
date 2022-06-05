2399526ce760935cac6cf4d5a2e63b3c0993131d
Moon-Ga <dev.jaeseok@gmail.com>
2022-06-05 12:23:25 +0900

    Netlify proxy

--- src/utils/getHero.ts
+++ src/utils/getHero.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const PROXY = window.location.hostname === 'localhost' ? '' : 'proxy';
+
 const getHero = async (heroId: number) => {
   try {
     const res = await axios.get<HeroInfo>(
-      `api/${process.env.REACT_APP_SUPERHERO_API_KEY}/${heroId}`
+      `${PROXY}/api/${process.env.REACT_APP_SUPERHERO_API_KEY}/${heroId}`
     );
     return res.data;
   } catch (error) {
